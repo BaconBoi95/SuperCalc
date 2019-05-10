7077f51d2d12dd7e590e750b75978fba2f88dd2f
BaconBoi95 <leeland230.fun@gmail.com>
2019-05-10 10:28:52 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include "lib\calculators\basic.cpp"
+#include "lib\calculators\gradecalc.cpp"
+#include "lib\calculators\statmain.cpp"
 int main() {
-  basiccalc();
+  int a;
+  cout << "[1] Basic Calculator" << endl << "[2] Grade Calculator" << endl << "[3] Statistic Calculator";
+  cin >> a;
+  if(a==1) {
+    basiccalc();
+  }
+  else if(a==2) {
+    grade();
+  }
+  else if(a==3) {
+    statisticalcalc();
+  }
 };
